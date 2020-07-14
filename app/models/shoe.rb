05559f9692f63f19dbd41828b5f54e8f3a624a7d
hansenjl <jenn.leigh.hansen@gmail.com>
2020-07-14 07:54:50 -0700

    study group setup

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -3,7 +3,7 @@ class Shoe < ApplicationRecord
     def self.ordered_by_price
         #self.all.sort_by{|shoe| shoe.price}.last
         self.order(price: :desc)
-        
+
     end
 
 
@@ -11,14 +11,4 @@ class Shoe < ApplicationRecord
         "#{self.brand} - #{self.color}"
     end
 
-    def display_price
-        split_price = self.price.to_s.split(".")
-        if split_price[1].length == 1
-            split_price[1] << "0"
-        elsif split_price[1].length == 0
-            split_price[1] << "00"
-        end
-        display = split_price.join(".")
-        "$#{display}"
-    end
 end
