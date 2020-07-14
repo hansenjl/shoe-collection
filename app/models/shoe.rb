6573c781fc01850e80170c901256975a7e159eb4
hansenjl <jenn.leigh.hansen@gmail.com>
2020-07-14 09:05:43 -0700

    fix confirmation validation

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -2,7 +2,7 @@ class Shoe < ApplicationRecord
     validates :price, :brand, :color, presence: true
     validates :price,  numericality: {greater_than: 0, message: "must be of some value, silly!"}
     validates :color, uniqueness: {scope: [:brand, :price], message: "with that brand & price has already been added"}
-    validates :price, confirmation: true, on: create
+    validates :price, confirmation: true, on: :create
     validates :price_confirmation, presence: true
     validates :color, :brand, two_word: true
 
