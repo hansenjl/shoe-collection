53e516b7718c06d3b6dc508d7c8a6ad7c6f86d17
hansenjl <jenn.leigh.hansen@gmail.com>
2020-07-14 23:07:13 -0700

    remove brand validations

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -1,16 +1,15 @@
 class Shoe < ApplicationRecord
-    validates :price, :brand, :color, presence: true
+    validates :price, :color, presence: true
     validates :price,  numericality: {greater_than: 0, message: "must be of some value, silly!"}
-    validates :color, uniqueness: {scope: [:brand, :price], message: "with that brand & price has already been added"}
+    validates :color, uniqueness: {scope: :price, message: "with that price has already been added"}
     validates :price, confirmation: true, on: :create
     validates :price_confirmation, presence: true
-    validates :color, :brand, two_word: true
+    validates :color, two_word: true
 
 
     def self.ordered_by_price
         #self.all.sort_by{|shoe| shoe.price}.last
         self.order(price: :desc)
-
     end
 
 
